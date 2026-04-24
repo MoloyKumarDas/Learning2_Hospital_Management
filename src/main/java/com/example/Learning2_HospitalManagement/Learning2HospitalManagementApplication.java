eceacfd7669af75b4d4defdf63df0371d526c42d
MoloyKumarDas <moloykumardas10@gmail.com>
2026-04-24 20:30:31 +0600

    initial set up // just changed properties for database connection

---
+++ src/main/java/com/example/Learning2_HospitalManagement/Learning2HospitalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.Learning2_HospitalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning2HospitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning2HospitalManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Learning2_HospitalManagement/Learning2HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Learning2_HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learning2HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
