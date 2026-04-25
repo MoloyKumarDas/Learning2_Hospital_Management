bb58ea5adf8a1322b29075ef9eacafb616456ae3
MoloyKumarDas <moloykumardas10@gmail.com>
2026-04-25 19:29:19 +0600

    now same method don't call by hybernate // before hybernate call it checks if same method previously called // check if same method stored in persistenceContext // if stored then just return the result without hybernate calling // @Transactional

--- src/main/java/com/example/Learning2_HospitalManagement/service/PatientService.java
+++ src/main/java/com/example/Learning2_HospitalManagement/service/PatientService.java
@@ -2,6 +2,7 @@ package com.example.Learning2_HospitalManagement.service;
 
 import com.example.Learning2_HospitalManagement.entity.Patient;
 import com.example.Learning2_HospitalManagement.repository.PatientRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,13 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
 
+    @Transactional                  // rollback if not execute completely      // if error->rollback
     public Patient getPatientById(Long id){
 
         Patient p1=patientRepository.findById(id).orElseThrow();
         Patient p2=patientRepository.findById(id).orElseThrow();
+
+        System.out.println(p1==p2);
 
         return p1;
     }
